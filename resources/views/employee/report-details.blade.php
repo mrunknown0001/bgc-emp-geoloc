98b3513db8ae0ddac93ee616e96cd1442e33d81f
Adam <madamt0001@gmail.com>
2021-12-14 08:36:15 +0800

    Update

--- resources/views/employee/report-details.blade.php
+++ resources/views/employee/report-details.blade.php
@@ -31,6 +31,7 @@
 				@include('includes.all')
 			</div>
 			<div class="col-md-8">
+				<p><a href="{{ route('reports') }}" class="btn btn-primary btn-xs"><i class="fa fa-arrow-left"></i> Back to Reports</a></p>
 				<p>Location: <strong>{{ $data->farm->code }} - {{ $data->cat == 'loc' ? $data->loc->location_name : $data->sub->location->location_name . ' - ' . $data->sub->sub_location_name }}</strong></p>
 				<p>Date &amp; Time: <strong>{{ date('F j, Y h:i:s A', strtotime($data->created_at)) }}</strong></p>
 				<p>Remarks: <strong>{{ $data->remarks }}</strong></p>
